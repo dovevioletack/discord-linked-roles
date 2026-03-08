c1253b9119a70f45fd6e4b806663e53aab0b04ae
dovevioletack <dovevioletack@proton.me>
2026-03-07 22:14:01 -0800

    Update dataMsg.ts

--- dataMsg.ts
+++ dataMsg.ts
@@ -1,7 +1,7 @@
 import type { TextChannel } from "discord.js";
 import { client } from "./client.js";
 
-export const dataMsg = await ((await client.channels.fetch("1310828248564764722")) as TextChannel).messages.fetch("1310828667697500242")
+export const dataMsg = await ((await client.channels.fetch(process.env.guild)) as TextChannel).messages.fetch(process.env.channel)
 export const data = JSON.parse(await (await fetch([...dataMsg.attachments.values()][0].url)).text())
 export const saveData = async () => await dataMsg.edit({
     "files": [
