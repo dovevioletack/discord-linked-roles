098056cfbd1d954f6245495946d77726e685c3e1
dovevioletack <dovevioletack@proton.me>
2026-03-07 22:28:53 -0800

    Update oauth2.ts

--- oauth2.ts
+++ oauth2.ts
@@ -16,7 +16,7 @@ import * as storage from './storage.ts';
  */
 
 const config = {
-  "DISCORD_CLIENT_ID": "1310810269232005202",
+  "DISCORD_CLIENT_ID": process.env.DISCORD_CLIENT_ID,
   "DISCORD_CLIENT_SECRET": process.env.DISCORD_CLIENT_SECRET as string,
   "DISCORD_REDIRECT_URI": "https://mijovia-linked-roles.onrender.com/discord-oauth-callback"
 }
